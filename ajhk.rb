a43821906076525a094435bd7eeffe22cfe9b658
Andrew Henning-Kolberg <akolberg@squaremouth.com>
2019-04-09 10:32:45 -0400

    add binding controls

--- ajhk.rb
+++ ajhk.rb
@@ -11,12 +11,38 @@
 
 class Ajhk
   @c = 0
+  @binding_enabled = true
 
   class << self
-    attr_accessor :c
+    attr_accessor :c, :binding_enabled
   end
 
   def self.reset_c
     @c = 0
+  end
+
+  def self.next_c
+    @c += 1
+  end
+
+  def self.bind
+    binding.remote_pry if self.binding_enabled?
+  end
+
+  def self.bind?
+    # Alias for `self.binding_enabled?`
+    self.binding_enabled?
+  end
+
+  def self.binding_enabled?
+    @binding_enabled
+  end
+
+  def self.enable_binding
+    @binding_enabled = true
+  end
+
+  def self.disable_binding
+    @binding_enabled = false
   end
 end
