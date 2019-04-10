e3e277f092960ad96faec5c5bce8383099cb12f7
Andrew Henning-Kolberg <akolberg@squaremouth.com>
2019-04-09 22:59:59 -0400

    add parameters to some C methods

--- ajhk/c.rb
+++ ajhk/c.rb
@@ -1,5 +1,7 @@
 module Ajhk
   class C
+    # Counter
+
     @c = 0
     @prefix = "AJHK: "
 
@@ -7,20 +9,25 @@ module Ajhk
       attr_accessor :c
     end
 
-    def self.reset(c = 0)
+    def self.reset(c=0)
       @c = c
     end
 
-    def self.next
-      @c += 1
+    def self n(n=1)
+      # Alias for .next
+      self.next(n)
     end
 
-    def self.prefix(prefix = "")
+    def self.next(n=1)
+      @c += n
+    end
+
+    def self.prefix(prefix="")
       @prefix = prefix
     end
 
     def self.to_s
-      "#{@prefix}C=#{@c}"
+      "#{@prefix}C.c=#{@c}"
     end
   end
 end
